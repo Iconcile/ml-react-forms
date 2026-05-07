53f802cc0e9965e834a152e817ac4af7499f2821
iconcileShivam <132429658+iconcileShivam@users.noreply.github.com>
2026-05-07 21:06:50 +0530

    fix style

--- dist/lib/ml-form-builder/index.js
+++ dist/lib/ml-form-builder/index.js
@@ -1,5 +1,5 @@
 var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
+    __assign = Object.assign || function (t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
             s = arguments[i];
             for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
@@ -55,7 +55,7 @@ export var BuildFormRow = function (props) {
     var columnItems = (get(schema, 'columns'));
     var rowSettings = __assign(__assign({}, settings), (get(schema, 'settings') || {}));
     var colItems = (isArray(schema) ? schema : ((isArray(columnItems) ? columnItems : [schema])));
-    var rowStyle = __assign({ marginBottom: (rowSettings.verticalSpacing !== null && rowSettings.verticalSpacing !== void 0 ? rowSettings.verticalSpacing : 10), display: 'flex' }, rowSettings.rowStyles);
+    var rowStyle = { marginBottom: (rowSettings.verticalSpacing !== null && rowSettings.verticalSpacing !== void 0 ? rowSettings.verticalSpacing : 10), display: 'flex' };
     var doNotHaveMoreElements = function (index) {
         return filter(colItems.slice(index + 1), function (item) {
             var componentConfig = ComponentMapConfig[item.type];
@@ -73,7 +73,7 @@ export var BuildFormRow = function (props) {
         var Component = componentConfig.component;
         if (conditionalProps.hidden === true)
             return null;
-        return (React.createElement("div", { key: "".concat(rowId, "_field_").concat(index), className: item.classNames, style: __assign({ flex: (item.flex !== null && item.flex !== void 0 ? item.flex : 1), marginRight: horizontalSpacing, paddingLeft: rowSettings.columnHorizontalPadding, paddingRight: rowSettings.columnHorizontalPadding }, item.styles) }, (settings.isReadOnly && item.readOnlyProps && isFunction(item.readOnlyProps.renderer)) ?
+        return (React.createElement("div", { key: "".concat(rowId, "_field_").concat(index), className: item.classNames, style: __assign({ flex: item.flex ?? 1, marginRight: horizontalSpacing, paddingLeft: rowSettings.columnHorizontalPadding, paddingRight: rowSettings.columnHorizontalPadding }, item.styles) }, (settings.isReadOnly && item.readOnlyProps && isFunction(item.readOnlyProps.renderer)) ?
             (item.readOnlyProps.renderer({ formikProps: formikProps, fieldConfig: item, isReadOnly: settings.isReadOnly })) :
             ((item === null || item === void 0 ? void 0 : item.isFastField) === true ? (React.createElement(FastField, { name: (item.name || item.valueKey) }, function (fastFieldProps) {
                 var mergedFieldProps = __assign(__assign({}, fieldProps), { field: fastFieldProps.field, meta: fastFieldProps.meta, form: fastFieldProps.form });
@@ -104,13 +104,17 @@ export var MLFormAction = function (props) {
     var formId = props.formId, _a = props.formikProps, formikProps = _a === void 0 ? {} : _a, containerClassNames = props.containerClassNames, _b = props.submitButtonLayout, submitButtonLayout = _b === void 0 ? 'center' : _b, _c = props.submitButtonText, submitButtonText = _c === void 0 ? "Submit" : _c, submitButtonProps = props.submitButtonProps;
     if (props.actionContent)
         return (React.cloneElement(props.actionContent || React.createElement("div", null), { formikProps: formikProps }));
-    var ActionContainer = styled('div')(function () { return (__assign(__assign(__assign({ position: 'relative', display: 'flex', justifyContent: 'center' }, (submitButtonLayout === 'center' && {
-        justifyContent: 'center'
-    })), (submitButtonLayout === 'right' && {
-        justifyContent: 'flex-end'
-    })), { '&.action-fullWidth > button': {
-            flex: 1
-        } })); });
+    var ActionContainer = styled('div')(function () {
+        return (__assign(__assign(__assign({ position: 'relative', display: 'flex', justifyContent: 'center' }, (submitButtonLayout === 'center' && {
+            justifyContent: 'center'
+        })), (submitButtonLayout === 'right' && {
+            justifyContent: 'flex-end'
+        })), {
+            '&.action-fullWidth > button': {
+                flex: 1
+            }
+        }));
+    });
     return (React.createElement(ActionContainer, { className: containerClassNames }, (props.actionContent) ?
         (React.cloneElement(props.actionContent || React.createElement("div", null), { formikProps: formikProps, formId: formId }))
         : (React.createElement(LoadingButton, __assign({ loading: formikProps.isSubmitting, type: "submit", disabled: formikProps.isSubmitting, variant: "contained", color: "primary" }, submitButtonProps), submitButtonText))));
@@ -124,6 +128,6 @@ export var MLFormBuilder = function (props) {
     return (React.createElement("form", { onSubmit: formikProps.handleSubmit },
         React.createElement(MLFormContent, __assign({}, props)),
         (actionConfig.displayActions !== false) &&
-            (React.createElement(MLFormAction, __assign({ formId: props.formId, formikProps: formikProps }, actionConfig)))));
+        (React.createElement(MLFormAction, __assign({ formId: props.formId, formikProps: formikProps }, actionConfig)))));
 };
 export default MLFormBuilder;
